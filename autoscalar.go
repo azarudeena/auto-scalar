16c2dc6c6ad7078297087410b0d106481016fb39
azarudeena <2azarinbox@gmail.com>
2023-10-16 20:49:26 +0530

    types for requests and response

--- autoscalar.go
+++ autoscalar.go
@@ -8,6 +8,15 @@ import "fmt"
 //		inc replica will dec CPU and dec replica will inc CPU
 // call /app/replicas to PUT the new replica count
 
+type AppStatus struct {
+	CPU      map[string]float64 `json:"cpu"`
+	Replicas int                `json:"replicas"`
+}
+
+type Replicas struct {
+	Replicas int `json:"replicas"`
+}
+
 func main() {
 
 	fmt.Println("Idea to Code")
